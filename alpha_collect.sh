f1113014c745259fc990bb31d63bb3de1dbe9901
itsMeOnli <aman.mohammed.818@gmail.com>
2026-04-06 22:44:28 +0300

    started alpha_collect.sh

---
+++ alpha_collect.sh
@@ -0,0 +1,18 @@
+ #!/usr/bin/env bash
+
+CONFIG="config/settings.json"
+
+PEER_HOST=$(jq -r '.peer_hostname' "$CONFIG")
+
+DATA_DIR=$(jq -r  '.data_dir' "$CONFIG" | sed "s|~|$HOME|g")
+LOG_DIR=$(jq -r   '.log_dir' "$CONFIG" | sed "s|~|$HOME|g")
+
+#if directories do not exist
+mkdir -p "$DATA_DIR"
+mkdir -p "$LOG_DIR"
+mkdir -p "$HOME/exchange/outbox"
+
+#intialize metrics.json if it doesn't exist
+if [[ ! -f "$DATA_DIR/metrics.json" ]]; then
+    echo '[]' > "$DATA_DIR/metrics.json"
+fi
