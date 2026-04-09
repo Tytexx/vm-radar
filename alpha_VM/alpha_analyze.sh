e76c5c9aa13a39835099d3153ad9cd4171cf6518
itsMeOnli <aman.mohammed.818@gmail.com>
2026-04-10 00:58:35 +0300

    alpha_analyze.sh takes values and thresholds

---
+++ alpha_VM/alpha_analyze.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#take values from config
+CONFIG="config/settings.json"
+THRESHOLDS="config/thresholds.json"
+
+INTERVAL=$(jq -r '.collect_interval_sec' "$CONFIG")
+REDIS_HOST=$(jq -r '.redis_host' "$CONFIG")
+DATA_DIR=$(jq -r '.data_dir' "$CONFIG" | sed "s|~|$HOME|g")
+LOG_DIR=$(jq -r '.log_dir' "$CONFIG" | sed "s|~|$HOME|g")
+
+#take thresholds
+CPU_WARN=$(jq -r '.cpu_warn' "$THRESHOLDS")
+CPU_CRIT=$(jq -r '.cpu_crit' "$THRESHOLDS")
+MEM_WARN=$(jq -r '.mem_warn' "$THRESHOLDS")
+MEM_CRIT=$(jq -r '.mem_crit' "$THRESHOLDS")
+DISK_WARN=$(jq -r '.disk_warn' "$THRESHOLDS")
+DISK_CRIT=$(jq -r '.disk_crit' "$THRESHOLDS")
+
